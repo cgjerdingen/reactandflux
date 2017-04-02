0224334b1f4a363fb29d58145706518f490aeb8e
Craig Gjerdingen <gjer0005@umn.edu>
2017-04-02 17:57:05 -0500

    add babel parser. added react, react-dom. built our first homePage component

---
+++ src/components/homePage.js
@@ -0,0 +1,13 @@
+//"use strict";
+
+var React = require('React');
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className="jumbotron"><h1>Course Administration</h1></div>
+		);
+	}
+});
+
+module.exports = Home;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+//var test = 'testing';
+var Home = require('./components/homePage');
 
-var App = console.log('Hello world from Browserify');
 
-module.exports = App;
+//var Home = React.createClass({
+//	render: function() {
+//		return (
+//			<div>Administration</div>
+//		);
+//	}
+//});
+
+
+ReactDOM.render(<Home />, document.getElementById('app'));
+
+
+
+//var App = console.log('Hello world from Browserify');
+
+//module.exports = App;
+
+
+
+
